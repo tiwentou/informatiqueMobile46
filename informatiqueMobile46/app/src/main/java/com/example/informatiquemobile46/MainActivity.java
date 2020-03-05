f5a60e31e06f385650c45ddb28d408f96ae6a62d
tiwentou <57150227+tiwentou@users.noreply.github.com>
2020-03-05 18:57:03 -0500

    create questionnaire layout

--- informatiqueMobile46/app/src/main/java/com/example/informatiquemobile46/MainActivity.java
+++ informatiqueMobile46/app/src/main/java/com/example/informatiquemobile46/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.informatiquemobile46;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "notif";
+    public static final int TEXT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+
+        Log.i(TAG, "Vers l'infini");
+
+        Intent intent = new Intent(this, Questionnaire.class);
+
+        startActivityForResult(intent, TEXT_REQUEST);
+
+    }
+
+    public void goToQuestionnaire(View view) {
+        Intent intent = new Intent(this, Questionnaire.class);
+
+        startActivityForResult(intent, TEXT_REQUEST);
+
     }
 }
---
+++ informatiqueMobile46/app/src/main/java/com/example/informatiquemobile46/Questionnaire.java
@@ -0,0 +1,14 @@
+package com.example.informatiquemobile46;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Questionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire);
+    }
+}
